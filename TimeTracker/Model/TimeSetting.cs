1e3d117a0275e186241eee43f2afd0dadb8341bd
bonncherchez <bonn.cherchez@ectinfo.com>
2013-12-03 15:26:23 +0800

    Change Time Selection in JobTrack.aspx

---
+++ TimeTracker/Model/TimeSetting.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace TimeTracker.Model
+{
+    public class TimeSetting : T_TimeSetting
+    {
+        public TimeSetting GetTimeSetting()
+        {
+            TimeTrackerEntities db = new TimeTrackerEntities();
+
+            var data = (from t in db.T_TimeSetting
+                        select new TimeSetting()
+                        {
+                            Id = t.Id,
+                            Interval = t.Interval,
+                            LastUpdateDate = t.LastUpdateDate,
+                            LastUpdatedBy = t.LastUpdatedBy
+                        }).FirstOrDefault();
+
+            db.Dispose();
+
+            return data;
+        }
+
+        public TimeSetting GetTimeSetting(int id)
+        {
+            TimeTrackerEntities db = new TimeTrackerEntities();
+
+            var data = (from t in db.T_TimeSetting
+                        where t.Id == id
+                        select new TimeSetting()
+                        {
+                            Id = t.Id,
+                            Interval = t.Interval,
+                            LastUpdateDate = t.LastUpdateDate,
+                            LastUpdatedBy = t.LastUpdatedBy
+                        }).FirstOrDefault();
+
+            db.Dispose();
+
+            return data;
+        }
+    }
+}
